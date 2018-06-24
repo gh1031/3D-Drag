c647718237c9d09c9e5be444cfb37cc6d68a9a8e
gh1031 <1051421758@qq.com>
2018-06-24 18:51:57 +0800

    处理移动端兼容

--- js/drag.js
+++ js/drag.js
@@ -3,30 +3,42 @@
  * @desc make img dragable
  */
 
- $(function () {
+$(function () {
   var imgL = $('.wrap img').length;
   var deg = 360 / imgL;
   var rotY = 0;
   var rotX = -10;
+  var eventstart = 'mousedown';
+  var eventmove = 'mousemove';
+  var eventend = 'mouseup';
+  var isMobile = false;
   $('html').attr('onselectstart', 'return false')
-  $('.wrap img').each(function(i) {
+  $('.wrap img').each(function (i) {
     $(this).css({
-      'transform': 'rotateY(' + i * deg + 'deg) translateZ(' + 350 +'px)'
+      'transform': 'rotateY(' + i * deg + 'deg) translateZ(' + 350 + 'px)'
     }).attr('ondragstart', 'return false');
     // 拖动事件
-    $(document).mousedown(function(e) {
-      console.log('down')
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|MQQBrowser|JUC|Windows Phone)/i))) {
+      eventstart = 'touchstart';
+      eventmove = 'touchmove';
+      eventend = 'touchend';
+      isMobile = true;
+    }
+    $(document).on(eventstart, function (e) {
+      // 处理移动端兼容
+      e = isMobile ? e.originalEvent.touches[0] : e;
       var prevx = e.clientX;
       var prevY = e.clientY;
-      $(this).on('mousemove', function(e) {
-        // $(document.body).append(`
-        //   <div style="width:10px; height:10px; background:#f00; position:absolute; left:${e.clientX}px;top: ${e.clientY}px">
-        //   </div>
-        // `)
+      $(this).on(eventmove, function (e) {
+        // 处理移动端兼容
+        e = isMobile ? e.originalEvent.touches[0] : e;
+        // 获取每次移动时鼠标的x， y坐标
         let x = e.clientX;
         let y = e.clientY;
+        // 获取与当前鼠标x，y轴与上次鼠标x，y轴之间的水平与垂直距离
         let xl = x - prevx;
         let yl = y - prevY;
+        // 将两次鼠标之间的距离以0.1倍赋值给wrap盒子水平和垂直的角度
         rotY += xl * 0.1;
         rotX -= yl * 0.1;
         $('.wrap').css({
@@ -35,10 +47,8 @@
         xl = e.clientX;
         yl = e.clientY;
       })
-    }).mouseup(function() {
-      console.log('up')
+    }).on(eventend, function () {
       $(this).off('mousemove')
     })
-
   })
- })
+})
