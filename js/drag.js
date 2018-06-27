0b432ffb85feb0c58c279003b4f131ede866733b
gh1031 <1051421758@qq.com>
2018-06-27 22:31:17 +0800

    todo: mouseup 触发多次

--- js/drag.js
+++ js/drag.js
@@ -12,11 +12,11 @@ $(function () {
   var eventmove = 'mousemove';
   var eventend = 'mouseup';
   var isMobile = false;
-  var xl, yl, play = null;
- 
-  $('.wrap img').each(function (i) {
+  var distX = distY = 0;
+  var play = null;
+  $('.wrap img').each(function (index) {
     $(this).css({
-      'transform': 'rotateY(' + i * deg + 'deg) translateZ(' + 400 + 'px)'
+      'transform': 'rotateY(' + index * deg + 'deg) translateZ(' + 350 + 'px)'
     }).attr('ondragstart', 'return false');
     // 拖动事件
     if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|MQQBrowser|JUC|Windows Phone)/i))) {
@@ -25,43 +25,46 @@ $(function () {
       eventend = 'touchend';
       isMobile = true;
     }
-    $(document).on(eventstart, function (e) {
+    $(document).bind(eventstart, function (e) {
+      clearInterval(play);
       // 处理移动端兼容
-      e = isMobile ? e.originalEvent.touches[0] : e;
-      var prevx = e.clientX;
-      var prevY = e.clientY;
-      $(this).on(eventmove, function (e) {
+      var ev = isMobile ? e.originalEvent.touches[0] : e;
+      var prevX = ev.clientX;
+      var prevY = ev.clientY;
+      $(this).bind(eventmove, function (e) {
         // 处理移动端兼容
-        e = isMobile ? e.originalEvent.touches[0] : e;
+        var ev = isMobile ? e.originalEvent.touches[0] : e;
         // 获取每次移动时鼠标的x， y坐标
-        let x = e.clientX;
-        let y = e.clientY;
+        var x = ev.clientX;
+        var y = ev.clientY;
         // 获取与当前鼠标x，y轴与上次鼠标x，y轴之间的水平与垂直距离
-        xl = x - prevx;
-        yl = y - prevY;
+        distX = x - prevX;
+        distY = y - prevY;
         // 将两次鼠标之间的距离以0.1倍赋值给wrap盒子水平和垂直的角度
-        rotY += xl * 0.1;
-        rotX -= yl * 0.1;
+        // console.log(distX, distY, '>>>>')
+        rotY += distX * 0.1;
+        rotX -= distY * 0.1;
         $('.wrap').css({
           transform: 'perspective(800px) translateX(-50%) translateY(-50%) rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)'
         })
         // 将当前的坐标付给上一个坐标
-        prevx = e.clientX;
-        prevY = e.clientY;
+        prevX = ev.clientX;
+        prevY = ev.clientY;
       })
-    }).on(eventend, function () {
-      $(this).off(eventmove);
-        play = setInterval(function() {
-          xl = xl * 0.95;
-          yl = yl * 0.95;
-          if (Math.abs(xl) < 0.5 && Math.abs(yl) < 0.5) {
-            clearInterval(play);
-          }
-          rotY += xl * 0.1;
-          rotX -= yl * 0.1;
-          $('.wrap').css({
-            transform: 'perspective(800px) translateX(-50%) translateY(-50%) rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)'
-          })
+    })
+    $(document).bind(eventend, function () {
+      $(this).unbind(eventmove);
+      play = setInterval(function () {
+        distX = distX * 0.95;
+        distY = distY * 0.95;
+        if (Math.abs(distX) <= 0.5 && Math.abs(distY) <= 0.5) {
+          clearInterval( play );
+        }
+        rotY += distX * 0.1;
+        rotX -= distY * 0.1;
+        $('.wrap').css({
+          transform: 'perspective(800px) translateX(-50%) translateY(-50%) rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)'
+        })
       }, 30)
     })
   })
