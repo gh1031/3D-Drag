d7d87ff32eea462c1dc62e5c55ca0733ccb71bb4
gh1031 <1051421758@qq.com>
2018-06-28 21:11:44 +0800

    add img

--- js/drag.js
+++ js/drag.js
@@ -3,6 +3,7 @@
  * @desc make img dragable
  */
 
+
 $(function () {
   var imgL = $('.wrap img').length;
   var deg = 360 / imgL;
@@ -14,26 +15,51 @@ $(function () {
   var isMobile = false;
   var distX = distY = 0;
   var play = null;
+  var text = {
+    0: '羞哒哒的小仙女',
+    1: '赛天仙的小仙女',
+    2: '敲可爱的小仙女',
+    3: '18岁的小仙女',
+    4: '睡觉觉的小仙女',
+    5: '有气质的小仙女',
+    6: '委屈巴巴的小仙女',
+    7: '萌哒哒的小仙女',
+    8: '超短发的小仙女',
+    9: '苗条的小仙女',
+    10: '圆嘟嘟的小仙女',
+    11: '诱人的小仙女',
+  }
   $('.wrap img').each(function (index) {
     $(this).css({
       'transform': 'rotateY(' + index * deg + 'deg) translateZ(' + 350 + 'px)'
-    }).attr('ondragstart', 'return false');
-    // 拖动事件
-    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|MQQBrowser|JUC|Windows Phone)/i))) {
-      eventstart = 'touchstart';
-      eventmove = 'touchmove';
-      eventend = 'touchend';
-      isMobile = true;
-    }
+    }).attr('ondragstart', 'return false').on('click', function() {
+      $('.mask').css('display', 'flex');
+      $('.mask img').attr('src', './img/img'+index+'.JPG');
+      $('.mask span').html(text[index + ''])
+      $('.mask .big-img').on('click', function() {
+        console.log($('.mask').css('display'));
+        if ($('.mask').css('display') === 'flex') {
+          $('.mask').css('display', 'none');
+        }
+        return;
+      })
+    })
   })
+  // 处理移动端兼容
+  if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|MQQBrowser|JUC|Windows Phone)/i))) {
+    eventstart = 'touchstart';
+    eventmove = 'touchmove';
+    eventend = 'touchend';
+    isMobile = true;
+  }
   // 拖拽事件
-  $(document).bind(eventstart, function (e) {
+  $(document).on(eventstart, function (e) {
     clearInterval(play);
     // 处理移动端兼容
     var ev = isMobile ? e.originalEvent.touches[0] : e;
     var prevX = ev.clientX;
     var prevY = ev.clientY;
-    $(this).bind(eventmove, function (e) {
+    $(this).on(eventmove, function (e) {
       // 处理移动端兼容
       var ev = isMobile ? e.originalEvent.touches[0] : e;
       // 获取每次移动时鼠标的x， y坐标
@@ -52,8 +78,8 @@ $(function () {
       prevX = ev.clientX;
       prevY = ev.clientY;
     })
-  }).bind(eventend, function () {
-    $(this).unbind(eventmove);
+  }).on(eventend, function () {
+    $(this).off(eventmove);
     play = setInterval(function () {
       distX = distX * 0.95;
       distY = distY * 0.95;
