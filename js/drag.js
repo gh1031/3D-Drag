d6168e900eb3950e5242c701773f731e3c860955
gh1031 <1051421758@qq.com>
2018-06-28 19:45:39 +0800

    fix a bug, because of my mouse event was writed in loop

--- js/drag.js
+++ js/drag.js
@@ -25,47 +25,47 @@ $(function () {
       eventend = 'touchend';
       isMobile = true;
     }
-    $(document).bind(eventstart, function (e) {
-      clearInterval(play);
+  })
+  // 拖拽事件
+  $(document).bind(eventstart, function (e) {
+    clearInterval(play);
+    // 处理移动端兼容
+    var ev = isMobile ? e.originalEvent.touches[0] : e;
+    var prevX = ev.clientX;
+    var prevY = ev.clientY;
+    $(this).bind(eventmove, function (e) {
       // 处理移动端兼容
       var ev = isMobile ? e.originalEvent.touches[0] : e;
-      var prevX = ev.clientX;
-      var prevY = ev.clientY;
-      $(this).bind(eventmove, function (e) {
-        // 处理移动端兼容
-        var ev = isMobile ? e.originalEvent.touches[0] : e;
-        // 获取每次移动时鼠标的x， y坐标
-        var x = ev.clientX;
-        var y = ev.clientY;
-        // 获取与当前鼠标x，y轴与上次鼠标x，y轴之间的水平与垂直距离
-        distX = x - prevX;
-        distY = y - prevY;
-        // 将两次鼠标之间的距离以0.1倍赋值给wrap盒子水平和垂直的角度
-        // console.log(distX, distY, '>>>>')
-        rotY += distX * 0.1;
-        rotX -= distY * 0.1;
-        $('.wrap').css({
-          transform: 'perspective(800px) translateX(-50%) translateY(-50%) rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)'
-        })
-        // 将当前的坐标付给上一个坐标
-        prevX = ev.clientX;
-        prevY = ev.clientY;
+      // 获取每次移动时鼠标的x， y坐标
+      var x = ev.clientX;
+      var y = ev.clientY;
+      // 获取与当前鼠标x，y轴与上次鼠标x，y轴之间的水平与垂直距离
+      distX = x - prevX;
+      distY = y - prevY;
+      // 将两次鼠标之间的距离以0.1倍赋值给wrap盒子水平和垂直的角度
+      rotY += distX * 0.2;
+      rotX -= distY * 0.1;
+      $('.wrap').css({
+        transform: 'perspective(800px) translateX(-50%) translateY(-50%) rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)'
       })
+      // 将当前的坐标付给上一个坐标
+      prevX = ev.clientX;
+      prevY = ev.clientY;
     })
-    $(document).bind(eventend, function () {
-      $(this).unbind(eventmove);
-      play = setInterval(function () {
-        distX = distX * 0.95;
-        distY = distY * 0.95;
-        if (Math.abs(distX) <= 0.5 && Math.abs(distY) <= 0.5) {
-          clearInterval( play );
-        }
-        rotY += distX * 0.1;
-        rotX -= distY * 0.1;
-        $('.wrap').css({
-          transform: 'perspective(800px) translateX(-50%) translateY(-50%) rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)'
-        })
-      }, 30)
-    })
+  }).bind(eventend, function () {
+    $(this).unbind(eventmove);
+    play = setInterval(function () {
+      distX = distX * 0.95;
+      distY = distY * 0.95;
+      if (Math.abs(distX) <= 0.5 && Math.abs(distY) <= 0.5) {
+        clearInterval( play );
+      }
+      console.log(distX, distY);
+      rotY += distX * 0.2;
+      rotX -= distY * 0.1;
+      $('.wrap').css({
+        transform: 'perspective(800px) translateX(-50%) translateY(-50%) rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)'
+      })
+    }, 30)
   })
 })
